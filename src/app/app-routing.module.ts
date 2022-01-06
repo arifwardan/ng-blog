9f2578528c1bae6ea85e3be455fe3375a8f3ec4d
Arif wardan <arif.wardan@integraldp.co.id>
2022-01-06 15:40:22 +0700

    c-1 #add-router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
+import { BlogComponent } from './blog/blog.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'blog', component: BlogComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
